dd3672f40c89721355412c43607f7ea1b81d6d7f
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-12 04:30:21 -0600

    finish exersice #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, repeatTimes) {
+    fullString = ""
+    if ( repeatTimes < 0) {
+        return `ERROR`
+    }
+    for (; repeatTimes > 0; repeatTimes--) {
+        fullString += string
+    }
+    return fullString
+}
 
 // Do not edit below this line
 module.exports = repeatString;
