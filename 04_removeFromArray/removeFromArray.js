4073f2ba941f7a8c97575c1ad65871372b511241
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-14 02:08:35 -0600

    finished exercise #4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    removedArray = []
+    for (let i = 0; i < array.length; i++)
+        if (!args.includes(array[i])) {
+            removedArray.push(array[i])
+        }
+    return removedArray
 };
 
 // Do not edit below this line
