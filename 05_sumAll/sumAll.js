0a8121dbef36f56e22ab44f92d3dfee289fc427b
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-14 02:51:32 -0600

    finish exercise #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    if (!Number.isInteger(firstInt) || !Number.isInteger(secondInt)) return "ERROR";
+    if (firstInt < 0 || secondInt < 0) return "ERROR";
+    if (firstInt > secondInt) {
+        const temp = firstInt
+        firstInt = secondInt
+        secondInt = temp
+    }
 
+    let totalSum = 0;
+
+    for (let i = firstInt; i <= secondInt; i++) {
+        totalSum += i
+    }
+    
+    return totalSum
 };
 
 // Do not edit below this line
