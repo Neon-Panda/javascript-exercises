9af247d10e6b0100bf4f30e10a7a2a7496658655
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-26 08:15:41 -0600

    finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const lettersOnly = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reverse = lettersOnly.split("").reverse().join("")
+    if (lettersOnly === reverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+palindromes("Hello")
 
 // Do not edit below this line
 module.exports = palindromes;
