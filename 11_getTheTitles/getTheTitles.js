f9692f74d09cbdcb277ef0fe73f5da09a8b22897
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-26 20:17:39 -0600

    finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const books = array.map((book) => book.title)
+    return books
 };
 
 // Do not edit below this line
