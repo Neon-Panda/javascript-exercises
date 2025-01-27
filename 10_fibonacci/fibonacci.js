36db14c823d23c9124643842ca57ed5d4a5c22e4
Neon-Panda <174306477+Neon-Panda@users.noreply.github.com>
2025-01-26 20:09:33 -0600

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = Number(number)
+    if (number < 0) {
+        return "OOPS"
+    }
+    if (number <= 1) {
+        return number;
+    }
+    let a = 0, b = 1, temp;
+    for (let i = 2; i <= number; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
